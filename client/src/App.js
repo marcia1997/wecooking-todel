9cf3eafa70cb42a6788bbb9b9ba06e33cc7ae700
marcia1997 <74060334+marcia1997@users.noreply.github.com>
2023-08-24 22:26:46 +0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -6,8 +6,7 @@ import Settings from "./pages/settings/Settings";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
 import { useContext } from "react";
-import { Context } from "./context/Context"
-//I take from reactrouter
+import { Context } from "./context/Context";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import axios from 'axios';
 
@@ -16,8 +15,8 @@ const instance = axios.create({
 });
 
 function App() {
+  const { user } = useContext(Context);
 
-  const {user} = useContext(Context);
   return (
     <Router>
       <Topbar />
@@ -28,13 +27,13 @@ function App() {
         <Route path="/posts">
           <Homepage />
         </Route>
-        <Route path="/register"> {user ? <Homepage /> : <Register />}</Route>
+        <Route path="/register">{user ? <Homepage /> : <Register />}</Route>
         <Route path="/login">{user ? <Homepage /> : <Login />}</Route>
         <Route path="/write">{user ? <Write /> : <Register />}</Route>
         <Route path="/settings">{user ? <Settings /> : <Register />}</Route>
         <Route path="/post/:postId">
           <Single />
-          </Route>
+        </Route>
       </Switch>
     </Router>
   );
