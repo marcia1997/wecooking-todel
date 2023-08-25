d199bc679def3e91d17284407e6ff3ea89dce07b
marcia1997 <74060334+marcia1997@users.noreply.github.com>
2023-08-25 21:28:38 +0300

    update indexjs deploy

--- api/index.js
+++ api/index.js
@@ -8,26 +8,29 @@ const categoryRoute = require("./routes/categories");
 const multer = require("multer");
 const path = require("path");
 
+// Create an instance of the Express application
+const app = express();
 
-//postman is going to be able to introduce json object inside  the body 
+// Load environment variables
 dotenv.config();
+
+// Middleware to parse JSON requests
 app.use(express.json());
+
+// Serve static files from the /images directory
 app.use("/images", express.static(path.join(__dirname, "/images")));
 
 const URI = process.env.MONGODB_URL;
 
 mongoose.connect(URI, {
-
-useNewUrlParser: true, 
-
-useUnifiedTopology: true 
-
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 }, err => {
-if(err) throw err;
-console.log('Connected to MongoDB!!!')
+  if (err) throw err;
+  console.log('Connected to MongoDB!!!');
 });
 
-//IMAGES
+// IMAGES
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "images");
@@ -42,7 +45,7 @@ app.post("/api/upload", upload.single("file"), (req, res) => {
   res.status(200).json("File has been uploaded");
 });
 
-//ROUTES
+// ROUTES
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
@@ -51,7 +54,3 @@ app.use("/api/categories", categoryRoute);
 app.listen("5000", () => {
   console.log("Backend is running.");
 });
-
-
-
-
