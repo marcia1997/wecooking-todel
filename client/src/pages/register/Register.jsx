e12869f2df608ae247e74020af88f501ce162def
marcia1997 <74060334+marcia1997@users.noreply.github.com>
2025-04-04 17:01:09 +0300

    Update Register.jsx

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -4,48 +4,68 @@ import axios from "axios";
 import "./register.css";
 
 export default function Register() {
-  const [username,setUsername] = useState("")
-  const [email,setEmail] = useState("")
-  const [password,setPassword] = useState("")
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [error, setError] = useState(false);
 
-  const handleSudmit = async (e) => {
-   e.preventDefault();
-   setError(false);
-   try{
-    const res = await axios.post("/auth/register", {
-      username,
-      email,
-      password,
-    });
-    res.data && window.location.replace("/login")
-   } catch (err){
-    setError(err);
-   }
+  const backendApiUrl = "https://wecooking-back.onrender.com"; // Asegúrate de usar la URL correcta del backend
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError(false);
+    try {
+      const res = await axios.post(`${backendApiUrl}/auth/register`, {
+        username,
+        email,
+        password,
+      });
+      res.data && window.location.replace("/login");
+    } catch (err) {
+      console.error("Error during registration:", err);
+      setError(true);
+    }
   };
-    return (
-        <div className="register">
+
+  return (
+    <div className="register">
       <span className="registerTitle">Register</span>
-      <form className="registerForm" onSubmit={handleSudmit}>
+      <form className="registerForm" onSubmit={handleSubmit}>
         <label>Username</label>
-        <input className="registerInput" type="text"
-         placeholder="Enter your username..."
-         onChange={e=>setUsername(e.target.value)} />
+        <input
+          className="registerInput"
+          type="text"
+          placeholder="Enter your username..."
+          onChange={(e) => setUsername(e.target.value)}
+        />
         <label>Email</label>
-        <input className="registerInput" type="text" 
-        placeholder="Enter your email..."
-        onChange={e=>setEmail(e.target.value)} />
+        <input
+          className="registerInput"
+          type="text"
+          placeholder="Enter your email..."
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
         <label>Password</label>
-        <input className="registerInput" type="password" 
-        placeholder="Enter your password..." 
-        onChange={e=>setPassword(e.target.value)}/>
-        <button className="registerButton" type="submit">Register</button>
+        <input
+          className="registerInput"
+          type="password"
+          placeholder="Enter your password..."
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button className="registerButton" type="submit">
+          Register
+        </button>
       </form>
       <button className="loginButton">
-     <Link to="/login">Login</Link>
-     </button>
-      {error && <span style={{color:"red", marginTop:"10px"}}>Something went wrong!</span>}
+        <Link to="/login">Login</Link>
+      </button>
+      {error && (
+        <span style={{ color: "red", marginTop: "10px" }}>
+          Something went wrong!
+        </span>
+      )}
     </div>
   );
 }
+
