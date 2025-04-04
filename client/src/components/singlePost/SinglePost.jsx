1d6a3baf8b56c4587c50f7adb9a0fb5452bfb7f2
marcia1997 <74060334+marcia1997@users.noreply.github.com>
2025-04-04 17:46:27 +0300

    Update SinglePost.jsx

--- client/src/components/singlePost/SinglePost.jsx
+++ client/src/components/singlePost/SinglePost.jsx
@@ -11,7 +11,7 @@ export default function SinglePost() {
   const [post, setPost] = useState({});
   
   const PF = process.env.NODE_ENV === 'production' 
-    ? "https://wecooking-back.onrender.com/images/" 
+    ? "https://wecooking-back.onrender.com/api/images/" 
     : "http://localhost:5000/images/";
 
   const { user } = useContext(Context);
