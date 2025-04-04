e2690b23062c0dcbb6ecd0692deeb24a4e92fce5
marcia1997 <74060334+marcia1997@users.noreply.github.com>
2025-04-04 17:04:03 +0300

    Update Home.jsx
    with host url

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,34 +1,37 @@
 import Header from "../../components/header/Header";
 import Posts from "../../components/posts/Posts";
 import Sidebar from "../../components/sidebar/Sidebar";
-import "./home.css"
-//fetch
-import axios from "axios"
-import { useEffect,useState } from "react";
+import "./home.css";
+// fetch
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router";
 
-export default function Home(){
-    const [posts,setPosts] = useState([]);
-    const {search} = useLocation();
-    
+const backendApiUrl = "https://wecooking-back.onrender.com";
 
+export default function Home() {
+  const [posts, setPosts] = useState([]);
+  const { search } = useLocation(); // captures the query from the URL
 
-    useEffect(() => {
-        const fetchPosts = async () => {
-         const res = await axios.get ("/posts"+search)
-         setPosts(res.data)
-        }
-        fetchPosts()
-    }, [search])
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const res = await axios.get(`${backendApiUrl}/posts${search}`);
+        setPosts(res.data);
+      } catch (err) {
+        console.error("Error fetching posts:", err);
+      }
+    };
+    fetchPosts();
+  }, [search]); // Rerun the effect if search changes
 
-    return (
-        <>
-        <Header/>  
-        <div className="home">
-         <Posts posts={posts} />
-         <Sidebar/>
-           
-        </div>
-        </>
-    );
+  return (
+    <>
+      <Header />
+      <div className="home">
+        <Posts posts={posts} />
+        <Sidebar />
+      </div>
+    </>
+  );
 }
