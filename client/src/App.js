e3528b9dd83b7204babf6d0a09081d1ad9698c80
marcia1997 <74060334+marcia1997@users.noreply.github.com>
2025-04-04 14:39:07 +0300

    general updates

--- client/src/App.js
+++ client/src/App.js
@@ -6,17 +6,21 @@ import Settings from "./pages/settings/Settings";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
 import axios from "axios";
-import { useContext } from "react";
-import { Context } from "./context/Context"
-//I take from reactrouter
+import { useEffect, useContext } from "react";
+import { Context } from "./context/Context";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
- 
+
 const backendApiUrl = "https://wecooking-back.onrender.com";
 
-
 function App() {
-  axios.get(`${backendApiUrl}/api/posts`)
-  const {user} = useContext(Context);
+  const { user } = useContext(Context);
+
+  useEffect(() => {
+    axios.get(`${backendApiUrl}/api/posts`)
+      .then(res => console.log("✅ Backend is working:", res.data))
+      .catch(err => console.error("❌Error:", err));
+  }, []);
+
   return (
     <Router>
       <Topbar />
@@ -27,13 +31,13 @@ function App() {
         <Route path="/posts">
           <Homepage />
         </Route>
-        <Route path="/register"> {user ? <Homepage /> : <Register />}</Route>
+        <Route path="/register">{user ? <Homepage /> : <Register />}</Route>
         <Route path="/login">{user ? <Homepage /> : <Login />}</Route>
         <Route path="/write">{user ? <Write /> : <Register />}</Route>
         <Route path="/settings">{user ? <Settings /> : <Register />}</Route>
         <Route path="/post/:postId">
           <Single />
-          </Route>
+        </Route>
       </Switch>
     </Router>
   );
