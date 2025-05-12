c1a04c817d59255cbef14d52cc62af1b320171ed
marcia1997 <74060334+marcia1997@users.noreply.github.com>
2025-05-12 23:42:27 +0300

    Cloudinary added

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,11 +1,17 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const Post = require("../models/Post");
+// Import the upload middleware
+const upload = require("../utils/upload");
 
-//CREATE POST
-router.post("/", async (req, res) => {
-  const newPost = new Post(req.body);
+// CREATE POST (now with image upload)
+router.post("/", upload.single("image"), async (req, res) => {
   try {
+    // Build the new Post object including the Cloudinary URL
+    const newPost = new Post({
+      ...req.body,
+      image: req.file.path, // public URL returned by Cloudinary
+    });
     const savedPost = await newPost.save();
     res.status(200).json(savedPost);
   } catch (err) {
@@ -13,51 +19,46 @@ router.post("/", async (req, res) => {
   }
 });
 
-//UPDATE POST
-router.put("/:id", async (req, res) => {
+// UPDATE POST (optionally update the image too)
+router.put("/:id", upload.single("image"), async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
-    if (post.username === req.body.username) {
-      try {
-        const updatedPost = await Post.findByIdAndUpdate(
-          req.params.id,
-          {
-            $set: req.body,
-          },
-          { new: true }
-        );
-        res.status(200).json(updatedPost);
-      } catch (err) {
-        res.status(500).json(err);
-      }
-    } else {
-      res.status(401).json("You can update only your post!");
+    if (post.username !== req.body.username) {
+      return res.status(401).json("You can update only your post!");
     }
+
+    // Prepare the update payload
+    const updateData = { ...req.body };
+    if (req.file) {
+      updateData.image = req.file.path; // update URL if a new image is provided
+    }
+
+    const updatedPost = await Post.findByIdAndUpdate(
+      req.params.id,
+      { $set: updateData },
+      { new: true }
+    );
+    res.status(200).json(updatedPost);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-//DELETE POST
+// DELETE POST
 router.delete("/:id", async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
-    if (post.username === req.body.username) {
-      try {
-        await post.delete();
-        res.status(200).json("Post has been deleted...");
-      } catch (err) {
-        res.status(500).json(err);
-      }
-    } else {
-      res.status(401).json("You can delete only your post!");
+    if (post.username !== req.body.username) {
+      return res.status(401).json("You can delete only your post!");
     }
+    await post.delete();
+    res.status(200).json("Post has been deleted...");
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-//GET POST
+// GET POST by ID
 router.get("/:id", async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
@@ -67,8 +68,7 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-//GET ALL POSTS
-//if you put /?cat=""
+// GET ALL POSTS (with optional filters)
 router.get("/", async (req, res) => {
   const username = req.query.user;
   const catName = req.query.cat;
@@ -78,9 +78,7 @@ router.get("/", async (req, res) => {
       posts = await Post.find({ username });
     } else if (catName) {
       posts = await Post.find({
-        categories: {
-          $in: [catName],
-        },
+        categories: { $in: [catName] },
       });
     } else {
       posts = await Post.find();
